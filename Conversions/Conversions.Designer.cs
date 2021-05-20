fac8ea5bc751edf24ab0566eaa80d57ad7664bb6
Kyle Jantzi <kylejantzi@outlook.com>
2021-05-20 17:11:35 -0400

    Add project files.

---
+++ Conversions/Conversions.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace Conversions
+{
+    partial class Conversions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.title = new System.Windows.Forms.Label();
+            this.conversion1 = new System.Windows.Forms.Label();
+            this.conversion3 = new System.Windows.Forms.Label();
+            this.conversion2 = new System.Windows.Forms.Label();
+            this.conversion4 = new System.Windows.Forms.Label();
+            this.convesionChoiceLabel = new System.Windows.Forms.Label();
+            this.valueConvertedLabel = new System.Windows.Forms.Label();
+            this.userChoice = new System.Windows.Forms.TextBox();
+            this.userValue = new System.Windows.Forms.TextBox();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.Font = new System.Drawing.Font("Palatino Linotype", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.title.Location = new System.Drawing.Point(52, 45);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(489, 38);
+            this.title.TabIndex = 0;
+            this.title.Text = "Imperial to Metric Conversions";
+            // 
+            // conversion1
+            // 
+            this.conversion1.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.conversion1.ForeColor = System.Drawing.Color.Black;
+            this.conversion1.Location = new System.Drawing.Point(143, 111);
+            this.conversion1.Name = "conversion1";
+            this.conversion1.Size = new System.Drawing.Size(282, 29);
+            this.conversion1.TabIndex = 1;
+            this.conversion1.Text = "1. Inches to Centimetres";
+            this.conversion1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // conversion3
+            // 
+            this.conversion3.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.conversion3.ForeColor = System.Drawing.Color.Black;
+            this.conversion3.Location = new System.Drawing.Point(143, 202);
+            this.conversion3.Name = "conversion3";
+            this.conversion3.Size = new System.Drawing.Size(282, 29);
+            this.conversion3.TabIndex = 2;
+            this.conversion3.Text = "3. Yards to Metres";
+            this.conversion3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // conversion2
+            // 
+            this.conversion2.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.conversion2.ForeColor = System.Drawing.Color.Black;
+            this.conversion2.Location = new System.Drawing.Point(143, 156);
+            this.conversion2.Name = "conversion2";
+            this.conversion2.Size = new System.Drawing.Size(282, 29);
+            this.conversion2.TabIndex = 3;
+            this.conversion2.Text = "2. Feet to Centimetres";
+            this.conversion2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // conversion4
+            // 
+            this.conversion4.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.conversion4.ForeColor = System.Drawing.Color.Black;
+            this.conversion4.Location = new System.Drawing.Point(143, 248);
+            this.conversion4.Name = "conversion4";
+            this.conversion4.Size = new System.Drawing.Size(282, 29);
+            this.conversion4.TabIndex = 4;
+            this.conversion4.Text = "4. Miles to Kilometres";
+            this.conversion4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // convesionChoiceLabel
+            // 
+            this.convesionChoiceLabel.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.convesionChoiceLabel.ForeColor = System.Drawing.Color.Black;
+            this.convesionChoiceLabel.Location = new System.Drawing.Point(89, 317);
+            this.convesionChoiceLabel.Name = "convesionChoiceLabel";
+            this.convesionChoiceLabel.Size = new System.Drawing.Size(282, 29);
+            this.convesionChoiceLabel.TabIndex = 5;
+            this.convesionChoiceLabel.Text = "Enter Conversion Choice (1-4):";
+            this.convesionChoiceLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // valueConvertedLabel
+            // 
+            this.valueConvertedLabel.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.valueConvertedLabel.ForeColor = System.Drawing.Color.Black;
+            this.valueConvertedLabel.Location = new System.Drawing.Point(89, 361);
+            this.valueConvertedLabel.Name = "valueConvertedLabel";
+            this.valueConvertedLabel.Size = new System.Drawing.Size(282, 29);
+            this.valueConvertedLabel.TabIndex = 6;
+            this.valueConvertedLabel.Text = "Enter Value to be Converted:";
+            this.valueConvertedLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // userChoice
+            // 
+            this.userChoice.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userChoice.Location = new System.Drawing.Point(377, 317);
+            this.userChoice.Name = "userChoice";
+            this.userChoice.Size = new System.Drawing.Size(100, 33);
+            this.userChoice.TabIndex = 7;
+            // 
+            // userValue
+            // 
+            this.userValue.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userValue.Location = new System.Drawing.Point(377, 361);
+            this.userValue.Name = "userValue";
+            this.userValue.Size = new System.Drawing.Size(100, 33);
+            this.userValue.TabIndex = 8;
+            // 
+            // convertButton
+            // 
+            this.convertButton.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.convertButton.Location = new System.Drawing.Point(94, 409);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(383, 32);
+            this.convertButton.TabIndex = 9;
+            this.convertButton.Text = "Convert";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(94, 464);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(383, 23);
+            this.outputLabel.TabIndex = 10;
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Conversions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(579, 505);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.userValue);
+            this.Controls.Add(this.userChoice);
+            this.Controls.Add(this.valueConvertedLabel);
+            this.Controls.Add(this.convesionChoiceLabel);
+            this.Controls.Add(this.conversion4);
+            this.Controls.Add(this.conversion2);
+            this.Controls.Add(this.conversion3);
+            this.Controls.Add(this.conversion1);
+            this.Controls.Add(this.title);
+            this.Name = "Conversions";
+            this.Text = "Conversions";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label conversion1;
+        private System.Windows.Forms.Label conversion3;
+        private System.Windows.Forms.Label conversion2;
+        private System.Windows.Forms.Label conversion4;
+        private System.Windows.Forms.Label convesionChoiceLabel;
+        private System.Windows.Forms.Label valueConvertedLabel;
+        private System.Windows.Forms.TextBox userChoice;
+        private System.Windows.Forms.TextBox userValue;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ Conversions/Conversions.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Conversions
+{
+    public partial class Conversions : Form
+    {
+        int conversion;
+        double inches;
+        double feet;
+        double yards;            
+        double miles;
+        double centimetres;
+        double metres;
+        double kilometres;
+
+        public Conversions()
+        {
+            InitializeComponent();
+        }
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            DisplayResults();
+        }
+        public void DisplayResults()
+        {
+            try
+            {
+                conversion = Convert.ToInt32(userChoice.Text);
+                if (conversion == 1)
+                {
+                    inches = Convert.ToDouble(userValue.Text);
+                    if (inches >= 0)
+                    {
+                        centimetres = inches * 2.54;
+                        outputLabel.Text = $"{inches} inches = {centimetres} centimetres";
+                    }
+                    else
+                    {
+                        userChoice.Text = "0";
+                        userValue.Text = "0";
+                        outputLabel.Text = $"Please Enter a Valid Number (1-4)";
+                    }
+                }
+                else if (conversion == 2)
+                {
+                    feet = Convert.ToDouble(userValue.Text);
+                    if (feet >= 0)
+                    {
+                        centimetres = feet * 30.48;
+                        outputLabel.Text = $"{feet} feet = {centimetres} centimetres";
+                    }
+                    else
+                    {
+                        userChoice.Text = "0";
+                        userValue.Text = "0";
+                        outputLabel.Text = $"Please Enter a Valid Number (1-4)";
+                    }
+                }
+                else if (conversion == 3)
+                {
+                    yards = Convert.ToDouble(userValue.Text);
+                    if (yards >= 0)
+                    {
+                        metres = yards * 0.91;
+                        outputLabel.Text = $"{yards} yards = {metres} metres";
+                    }
+                    else
+                    {
+                        userChoice.Text = "0";
+                        userValue.Text = "0";
+                        outputLabel.Text = $"Please Enter a Valid Number (1-4)";
+                    }
+                }
+                else if (conversion == 4)
+                {
+                    miles = Convert.ToDouble(userValue.Text);
+                    if (yards >= 0)
+                    {
+                        kilometres = miles * 1.6;
+                        outputLabel.Text = $"{miles} miles = {kilometres} kilometres";
+                    }
+                    else
+                    {
+                        userChoice.Text = "0";
+                        userValue.Text = "0";
+                        outputLabel.Text = $"Please Enter a Valid Number (1-4)";
+                    }
+                }
+                else
+                {
+                    userChoice.Text = "0";
+                    outputLabel.Text = "Please Enter a Valid Number (1-4)";
+                }
+            }
+            catch
+            {
+                userChoice.Text = "0";
+                userValue.Text = "0";
+                outputLabel.Text = $"Please Enter a Valid Number (1-4)";
+            }
+        }
+    }
+}
